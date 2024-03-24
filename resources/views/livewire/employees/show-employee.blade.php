aeebdf932ed1b0394034465baf2ea3f966fe84f7
Marcello Perri <webmarcello8080@gmail.com>
2024-03-24 16:58:24 +0000

    show employee started

--- resources/views/livewire/employees/show-employee.blade.php
+++ resources/views/livewire/employees/show-employee.blade.php
@@ -1,6 +1,15 @@
-<div>
-    <div class="flex mb-8 border-b border-gray/20">
-        <div></div>
+<div class="rounded-container">
+    <div class="flex items-center justify-between pb-8 mb-8 border-b border-gray/20">
+        <div class=" flex items-center gap-4">
+            <div class="rounded-lg w-24 f-24 overflow-hidden">
+                <img class="w-full h-full object-cover" src="{{ $employee->getMediaUrl('profile_image') }}" alt="">
+            </div>
+            <div>
+                <h5 class="mb-2">{{ $employee->getFullName() }}</h5>
+                <div class="mb-2 flex gap-2">@svg('briefcase', 'w-6 h-6') <span>{{ $employee?->employeeInformation->designation }}</span></div>
+                <div class="mb-2 flex gap-2">@svg('mail', 'w-6 h-6') <span>{{ $employee?->user->email }}</span></div>
+            </div>
+        </div>
         <a href="{{ route('employee.edit', $employee->id) }}" class="btn btn-big btn-purple">Edit Profile</a>
     </div>
 </div>
