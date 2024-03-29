757b4c66504ecc0c952b07fff778a665cd14d617
Marcello Perri <webmarcello8080@gmail.com>
2024-03-29 12:09:41 +0000

    menu and menu items and style for it

--- resources/views/partilas/side-menu.blade.php
+++ resources/views/partilas/side-menu.blade.php
@@ -1,7 +1,12 @@
 <div class="gray-container basis-72 grow-0 shrink-0">
-    <h2>LogoHere</h2>
-    <nav>
-        nav here
+    <h2 class="mb-8">LogoHere</h2>
+    <nav class="main-menu">
+        <a href="{{ route('employee.index' )}}" class="menu-item {{ request()->routeIs('employee.*') ? 'active' : '' }}">
+            @svg('group', 'w-6 h-6')<span>All Employees</span>
+        </a>
+        <a href="{{ route('components' )}}" class="menu-item {{ request()->routeIs('components') ? 'active' : '' }}">
+            @svg('eye', 'w-6 h-6')<span>Components</span>
+        </a>
     </nav>
     
     <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
