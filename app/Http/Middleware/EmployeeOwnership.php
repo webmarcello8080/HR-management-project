77e17e49d9f6c536317e002d6dc1ae3777ae4b4f
Marcello Perri <webmarcello8080@gmail.com>
2024-04-01 12:50:26 +0100

    better ownership middleware

--- app/Http/Middleware/EmployeeOwnership.php
+++ app/Http/Middleware/EmployeeOwnership.php
@@ -16,10 +16,14 @@ class EmployeeOwnership
     public function handle(Request $request, Closure $next): Response
     {
         // check if user is logged and it's an employee
-        if(auth()->check() && auth()->user()->hasRole('employee') && auth()->user()->employee && $request->route()->parameters()['id'] == auth()->user()->employee->id){
-            return $next($request);
+        if(auth()->check() && auth()->user()->hasRole('employee') && auth()->user()->employee){
+            // check if this route has an employee in it and if the employee ID matches with the AUTH employee ID
+            if($request->route('employee') && $request->route('employee')->id == auth()->user()->employee->id){
+                return $next($request);
+            }
         }
 
+        // ADMIN always has auth on this
         if(auth()->check() && auth()->user()->hasRole('admin')){
             return $next($request);
         }
