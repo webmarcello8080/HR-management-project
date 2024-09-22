ca71d820e48b8f823afb7ad0c42d80f5355a369a
Marcello Perri <webmarcello8080@gmail.com>
2024-09-22 11:52:49 +0100

    leggend in holiday table

--- resources/views/livewire/holidays/index-holiday.blade.php
+++ resources/views/livewire/holidays/index-holiday.blade.php
@@ -9,7 +9,7 @@
             <a class="btn btn-purple btn-big" wire:click="toggleModal">Add New Holiday</a>
         </div>
     </div>
-    <div class="table w-full border-collapse">
+    <div class="table w-full border-collapse mb-5">
         <div class="table-row border-b border-grey/20">
             <span class="caption table-cell py-2">Date</span>
             <span class="caption table-cell py-2">Day</span>
@@ -23,6 +23,10 @@
         @else
             <h6 class="my-5 caption">No holidays found</h6>
         @endif
+    </div>
+    <div class="flex flex-wrap gap-5 items-center font-bold">
+        <div class="flex items-center gap-2"><span class="bg-purple w-3 h-3 rounded-full"></span>Upcoming</div>
+        <div class="flex items-center gap-2"><span class="bg-grey/20 w-3 h-3 rounded-full"></span>Past holidays</div>
     </div>
     @if($showModal)
         @livewire('holidays.create-holiday', key('holiday-modal'))
