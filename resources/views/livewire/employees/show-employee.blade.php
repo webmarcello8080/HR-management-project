1c291f43cb21d4d479c23fbafc6dbe2561aa3863
Marcello Perri <webmarcello8080@gmail.com>
2024-09-22 10:46:20 +0100

    show payrolls in employee section

--- resources/views/livewire/employees/show-employee.blade.php
+++ resources/views/livewire/employees/show-employee.blade.php
@@ -6,7 +6,7 @@
         <div class="rounded-xl border border-grey/20 basis-[275px] grow-0 shrink-0 overflow-hidden">
             <div wire:click='setMenuTab(1)' class="menu-tabs @if($menuTab == 1) active @endif">@svg('user', 'w-6 h-6') <span>Profile</span></div>
             <div wire:click='setMenuTab(2)' class="menu-tabs @if($menuTab == 2) active @endif">@svg('calendar', 'w-6 h-6') <span>Attendance</span></div>
-            {{-- <div wire:click='setMenuTab(3)' class="menu-tabs @if($menuTab == 3) active @endif">@svg('file', 'w-6 h-6') <span>Projects</span></div> --}}
+            <div wire:click='setMenuTab(3)' class="menu-tabs @if($menuTab == 3) active @endif">@svg('dollar', 'w-6 h-6') <span>Payrolls</span></div>
             <div wire:click='setMenuTab(4)' class="menu-tabs @if($menuTab == 4) active @endif">@svg('notepad', 'w-6 h-6') <span>Leave</span></div>
         </div>
         <div class="grow">
@@ -17,7 +17,7 @@
                 @include('partials.attendances.attendance-table', ['attendances' => $employee->attendances, 'display_user' => false])
             @endif
             @if ($menuTab == 3)
-                <h4>something in here 3</h4>
+                @include('partials.payrolls.payroll-table', ['payrolls' => $employee->payrolls, 'display_user' => false])
             @endif
             @if ($menuTab == 4)
                 @include('partials.leaves.leave-table', ['leaves' => $employee->leaves, 'display_user' => false])
