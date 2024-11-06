3b17b37960bf4e61b64e5eb725639b039f524bf7
Marcello Perri <webmarcello8080@gmail.com>
2024-11-06 19:08:18 +0000

    head in a partial file to make the layout more standard

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,18 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>@yield('page-title') | {{ config('app.name') }}</title>
-
-        @if(app(App\Settings\GeneralSettings::class)->favicon)
-            <link rel="icon" href="{{ app(App\Settings\GeneralSettings::class)->favicon }}" type="image/x-icon">
-        @endif
-
-        @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-
-    </head>
+    @include('partials.head')
     <body>
         <main class="external-container h-[95vh] flex gap-8">
             <div class="basis-1/2 grow-0 shrink-0 rounded-2xl overflow-hidden">
